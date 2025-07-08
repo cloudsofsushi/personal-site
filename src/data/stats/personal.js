13b6b424b044b5ffb4872730302a5ab6d48eb70f
cloudsofsushi <sushmita7988@gmail.com>
2025-07-09 05:10:40 +0530

    updated the stats

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -5,7 +5,7 @@ const Age = () => {
 
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
+    const birthTime = new Date('2000-09-21T09:24:00');
     setAge(((Date.now() - birthTime) / divisor).toFixed(11));
   };
 
@@ -18,6 +18,19 @@ const Age = () => {
   return <>{age}</>;
 };
 
+const Coffee = () => {
+  const start = new Date('2025-07-01');
+  const end = Date.now();
+  let coffee = 2300;// coffee count on date 2025-07-01
+
+  while (end > start) {
+    coffee += 1;
+    start.setDate(start.getDate() + 1);
+  }
+
+  return coffee;
+};
+
 const data = [
   {
     key: 'age',
@@ -25,15 +38,49 @@ const data = [
     value: <Age />,
   },
   {
-    key: 'countries',
-    label: 'Countries visited',
-    value: 53,
-    link: 'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
+    key: 'city',
+    label: 'Current city',
+    value: 'Palwal',
   },
   {
-    key: 'location',
-    label: 'Current city',
-    value: 'New York, NY',
+    key: 'coffee',
+    label: 'Cups of chai/coffee',
+    value: <Coffee />,
+  },
+  {
+    key: 'FavBeverage',
+    label: 'Favorite beverage',
+    value: 'Black Current Shake',
+  },
+  {
+    key: 'MusicHours',
+    label: 'Weekly music hours',
+    value: 18,
+  },
+  {
+    key: 'AvgSleep',
+    label: 'Average sleep per night',
+    value: 6.8,
+  },
+  {
+    key: 'BooksThisYear',
+    label: 'Books read this year',
+    value: '12',
+  },
+  {
+    key: 'TotalBooks',
+    label: 'Total books read',
+    value: 86,
+  },
+  {
+    key: 'Cities',
+    label: 'Cities visited',
+    value: 4,
+  },
+  {
+    key: 'TopDestination',
+    label: 'Travel destination wish',
+    value: 'Switzerland',
   },
 ];
 
